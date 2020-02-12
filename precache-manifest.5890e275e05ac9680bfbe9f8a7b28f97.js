3465ddd8ca079a9185ca26b0f295be1e9c626f70
Veruska Rivera <veruskarivera@Veruskas-MacBook-Pro.local>
2020-02-12 10:05:46 -0500

    Updates

---
+++ precache-manifest.5890e275e05ac9680bfbe9f8a7b28f97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "152215cff9151c401a26562b88574a4c",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "5aa5af2236525b8e0bd9",
+    "url": "/dig4639-mobile-dev/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aa5af2236525b8e0bd9",
+    "url": "/dig4639-mobile-dev/static/js/main.0e1dd7b3.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
